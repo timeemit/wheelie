aebad588fba647375d3a5758b12b427f6989df14
TimeEmit <TimeEmit@sdf.lonestar.org>
2013-04-08 01:11:29 -0400

    Added github link.

--- test_app/javascripts/app.js
+++ test_app/javascripts/app.js
@@ -3,11 +3,11 @@ $(document).ready(function(){
 	$('#wild a').wheelie({radius:50, center_x:100, center_y:100});
 
 	$('#barrel_roll').click(function(){
-		$('#barrel_roll a').wheelie({radius:50, center_x:70, center_y:50}) 
+		$('#barrel_roll a').wheelie({radius:50, center_x:120, center_y:50}) 
 	});
 		
 	$('#clockify').click(function(){
-		$('#clockify span').wheelie({radius:50, center_x:70, center_y:50}, {radians_to_rotate_by: Math.PI / 6 + -Math.PI / 2}) 
+		$('#clockify span').wheelie({radius:50, center_x:70, center_y:60}, {radians_to_rotate_by: Math.PI / 6 - Math.PI / 2}) 
 	});
 
 	$('#collection a, #wild a').click(function(){
@@ -19,7 +19,6 @@ $(document).ready(function(){
 		else if ($this.parent().attr('id') == 'wild') {
 			selector = '#collection';
 		}
-		$this.detach();
 		$(selector).append($this).children('a').wheelie();
 	});	
 	
