e02dde28e242efde21e065607fb6c2606786f810
TimeEmit <TimeEmit@sdf.lonestar.org>
2013-04-08 09:50:12 -0400

    Configured deployment.  Finalized front-end.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,21 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "wheelie.js"
+set :repository,  "git@github.com:timeemit/DesignersCircus.git"
+set :ip_address, "50.57.115.223"
+set :deploy_to, "/home/oricksum/public_html/#{application}"
+set :user, "oricksum"
+set :port, 30000
+set :scm_username, "timeemit"
+set :scm, :git
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+default_run_options[:pty] = true
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+server ip_address, :app, :web
+role :db, ip_address, :primary => true
+
+# role :web, "your web-server here"                          # Your HTTP server, Apache/etc
+# role :app, "your app-server here"                          # This may be the same as your `Web` server
+# role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+# role :db,  "your slave db-server here"
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
@@ -15,11 +23,11 @@ role :db,  "your slave db-server here"
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  [:stop, :start, :restart, :reload].each do |action|
+    desc "#{action.to_s.capitalize} Apache"
+    task action, :roles => :web do
+      invoke_command "/etc/init.d/apache2 #{action.to_s}", :via => run_method
+    end
+  end
+end
